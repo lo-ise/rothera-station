bb8bf61b8330ff08b3804085079cf57e9e02b6e9
Louise Ireland <louireland@gmail.com>
2016-01-28 17:56:26 +0000

    Deleted - to retry with different settings

--- rothera_station_0.js
+++
@@ -1,5 +0,0 @@
-// Qgis2threejs Project
-project = new Q3D.Project({crs:"EPSG:32719",wgs84Center:{lat:-67.568522432,lon:-68.1233921729},proj:"+proj=utm +zone=19 +south +datum=WGS84 +units=m +no_defs",title:"rothera_station",baseExtent:[536333.887109,2504391.57562,538319.57973,2505561.20202],rotation:0,zShift:0.0,width:100.0,zExaggeration:1.5});
-
-// Layer 0
-lyr = project.addLayer(new Q3D.DEMLayer({q:1,shading:true,type:"dem",name:"rothera_dem_aoi"}));
